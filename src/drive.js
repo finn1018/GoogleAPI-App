9a3c6de606d1d99eacb7c13c8cdb670184c122f7
finn1018 <finn@coraise.co.kr>
2022-02-21 19:00:17 +0900

    2021

---
+++ src/drive.js
@@ -0,0 +1,14 @@
+import {React,useState} from 'react';
+import ReactDOM from 'react-dom';
+import './drive.css';
+import googleDrive from 'react-google-picker';
+
+function Drive() {
+    return (
+        <div id="driveBtn">
+            신규 고객사 등록
+        </div>
+    )
+}
+
+export default Drive;
--- src/index.js
+++ src/index.js
@@ -1,73 +1,23 @@
 import {React,useState} from 'react';
 import ReactDOM from 'react-dom';
-import {GoogleLogin, GoogleLogout} from 'react-google-login';
-
-const CLIENT_ID = 
-    "699515209239-4plnhkeavbuagngqafdr8sml6lei41i5.apps.googleusercontent.com";
+import Login from './login.js';
+import Drive from './drive.js';
+import './index.css';
 
 function App() {
-    /*State*/
-    let [isLoggedIn, setIsLoggedIn] = useState(false);
-    let [userInfo, setUserInfo] = useState({name:"",email:""});
-    /*******/
-
-    /*Success Handler*/
-    const responseGoogleSuccess = (response) => {
-        console.log(response+"success");
-        let userInfo = {
-            name: response.profileObj.name,
-            email: response.profileObj.email,
-        }
-        setUserInfo(userInfo);
-        setIsLoggedIn(true);
-    }
-    /*****************/
-
-    /*Error Handler*/
-    const responseGoogleError = (response) => {
-        console.log(response+"error");
-    }
-    /***************/
-
-    /*Logout Session and Update State*/
-    const logout = (response) => {
-        console.log(response);
-        let userInfo = {
-            name: "",
-            email: "",
-        };
-        setUserInfo(userInfo);
-        setIsLoggedIn(false);
-    }
-    /*********************************/
-
-    return(
-        <div className="row mt-5">
-        <div className="col-md-12">
-          {isLoggedIn ? (
-            <div>
-              <h1>Welcome, {userInfo.name}</h1>
-
-              <GoogleLogout
-                clientId={CLIENT_ID}
-                buttonText={"Logout"}
-                onLogoutSuccess={() => logout()}
-              ></GoogleLogout>
-            </div>
-          ) : (
-            <GoogleLogin
-              clientId={CLIENT_ID}
-              buttonText="Sign In with Google"
-              onSuccess={(response) => responseGoogleSuccess(response)}
-              onFailure={(response) => responseGoogleError(response)}
-              isSignedIn={true}
-              cookiePolicy={"single_host_origin"}
-            />
-          )}
-        </div>
-      </div>
-    )
+  return (
+    <>
+    <div id="header"> 
+      <h1 id="coraise">CORAISE</h1>
+      <Login/>
+    </div>
+    <div id="btns">
+      <Drive/>
+    </div>
+    </>
+  )
 }
+
 
 ReactDOM.render(
     <App/>,
